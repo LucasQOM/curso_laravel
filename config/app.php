1faa1800e26c2f9c4f490dd933b3f28cf7203ea3
Lucas Queiroz <lucasqueroz@hotmail.com>
2021-01-21 20:06:08 -0300

    teste

--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
         /*
          * Package Service Providers...
          */
-        //Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -226,7 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        //'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
